4658d4231a09a8f06ee8067be8fcc4d0e22aaf46
Thierry BM <beggars.banquet@gmail.com>
2014-08-05 21:02:31 -0400

    Classes placed in files

---
+++ DynamicXml/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace DynamicXml
+{
+	public class Person
+	{
+		[XmlElement("ageGoesHere")]
+		public double Age { get; set; }
+
+		[XmlElement("heightGoesThere")]
+		[XmlToString("N2")]
+		public double Height { get; set; }
+
+		public string Name { get; set; }
+
+		[XmlToString("yyyy-MM")]
+		public DateTime Date { get; set; }
+
+		public Person()
+		{
+			Age = 23 + Math.PI;
+			Height = 5 + 10.0 / 12.0;
+			Name = "Thierry";
+		}
+	}
+}
+
--- DynamicXml/Program.cs
+++ DynamicXml/Program.cs
@@ -19,36 +19,4 @@ namespace DynamicXml
 //            var tSerializer = new XmlSerializer(t);
         }
     }
-
-    public class Person
-    {
-        [XmlElement("ageGoesHere")]
-        public double Age { get; set; }
-
-        [XmlElement("heightGoesThere")]
-        [XmlToString("N2")]
-        public double Height { get; set; }
-
-        public string Name { get; set; }
-
-        [XmlToString("yyyy-MM")]
-        public DateTime Date { get; set; }
-
-        public Person()
-        {
-            Age = 23 + Math.PI;
-            Height = 5 + 10.0 / 12.0;
-            Name = "Thierry";
-        }
-    }
-
-    public class XmlToStringAttribute : Attribute
-    {
-        public string Argument { get; set; }
-
-        public XmlToStringAttribute(string argument)
-        {
-            Argument = argument;
-        }
-    }
 }
